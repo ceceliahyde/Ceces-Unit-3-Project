c6fd194e92bafc2be5ac55e3fb07ef6a19383ce1
ceceliahyde <126012434+ceceliahyde@users.noreply.github.com>
2023-02-21 10:27:59 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+let kerropiButton=document.querySelector(".kerropi");
+let optiontwoscreen=document.querySelector(".option-two-screen");
+let cinnamorollButton=document.querySelector(".cinnamoroll");
+let optiontwoend=document.querySelector(".option-two-end");
+let optiononescreen=document.querySelector(".option-one-screen");
+let secondButton=document.querySelector(".cinnamoroll2");
+let optiononeend=document.querySelector(".option-one-end");
+let Secondendbutton=document.querySelector(".theend");
+/* Declare variables below to save the different sections (divs) of your story*/
+kerropiButton.onclick=function(){
+    optiontwoscreen.style.display= "block";
+};
+
+
+
+
+
+
+
+
+cinnamorollButton.onclick=function(){
+    optiononescreen.style.display= "block";
+};
+
+
+
+secondButton.onclick=function(){
+    optiononeend.style.display= "block";
+};
+
+
+Secondendbutton.onclick=function(){
+    optiontwoend.style.display= "block";
+};
